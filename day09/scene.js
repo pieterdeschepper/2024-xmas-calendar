84b713b2cc3a0f66e8018bee6815b01675467b06
Pieter De Schepper <pieter.deschepper@timefold.ai>
2024-12-17 13:04:36 +0100

    feat: ux improvements

--- day09/scene.js
+++ day09/scene.js
@@ -1,7 +1,7 @@
 class Scene9 extends Scene2 {
 	constructor(canvas) {
 		super(canvas);
-		this.snowman = new Snowman(250, canvas.height - 220, 400);
+		this.snowman = new Snowman(250, canvas.height - 240, 400);
 	}
 
 	draw() {
