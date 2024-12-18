65737db95449992eb01e73cad8eb8c816e303782
Pieter De Schepper <pieter.deschepper@timefold.ai>
2024-12-18 12:04:37 +0100

    fix: stick performance

--- day07/stick.js
+++ day07/stick.js
@@ -5,9 +5,14 @@ class Stick {
 		this.angle = map(Math.random(), 0, 1, Math.PI / 12, Math.PI / 6);
 		this.len = size / 4;
 		this.rotation = rotation;
+		this.image = null;
 	}
 
-	draw(ctx) {
+	render(canvas) {
+		this.image = document.createElement("canvas");
+		this.image.width = canvas.width;
+		this.image.height = canvas.height;
+		const ctx = this.image.getContext("2d");
 		ctx.beginPath();
 		ctx.save();
 		ctx.strokeStyle = Color.black;
@@ -15,6 +20,13 @@ class Stick {
 		ctx.rotate(this.rotation);
 		this.branch(ctx, this.len, this.len * 0.07);
 		ctx.restore();
+	}
+
+	draw(ctx) {
+		if (!this.image) {
+			this.render(ctx.canvas);
+		}
+		ctx.drawImage(this.image, 0, 0);
 	}
 
 	branch(ctx, len, thickness) {
