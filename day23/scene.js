1f3e23581092160b3213ab550d72bb6e842ad85d
Pieter De Schepper <pieter.deschepper@timefold.ai>
2024-12-26 19:41:20 +0100

    fix: better draw order of last scene

--- day23/scene.js
+++ day23/scene.js
@@ -24,11 +24,45 @@ class Scene23 extends Scene22 {
 	}
 
 	draw() {
-		super.draw();
-		for (let s of this.smoke) {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+		for (const s of this.stars) {
+			s.draw(this.ctx);
+		}
+
+		this.moon.draw(this.ctx);
+
+		for (const s of this.snowflakes) {
+			s.draw(this.ctx);
+		}
+
+		for (const c of this.clouds) {
+			c.draw(this.ctx);
+		}
+
+		for (const s of this.smoke) {
 			s.draw(this.ctx);
 		}
 
 		this.cabin.draw(this.ctx);
+
+		for (const h of this.hills) {
+			h.draw(this.ctx);
+		}
+
+		this.tree.draw(this.ctx);
+		this.snowman.draw(this.ctx);
+
+		for (const h of this.hillsForeground) {
+			h.draw(this.ctx);
+		}
+
+		for (const c of this.cords) {
+			c.draw(this.ctx);
+		}
+
+		for (const b of this.bells) {
+			b.draw(this.ctx);
+		}
 	}
 }
